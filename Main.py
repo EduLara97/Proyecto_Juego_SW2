36c011821ccecff79710970b9715292dd1bc77b1
JulioReategui <jgra1993@gmail.com>
2017-11-26 22:50:30 -0500

    botones
    añadir tamaño letra micro, funcion botone y funcion añadir texto al boton.

--- Main.py
+++ Main.py
@@ -32,6 +32,7 @@ reloj = pg.time.Clock()
 
 display_ancho = 800
 display_altura = 600
+microfont = pg.font.SysFont("comicsansms", 10)
 pequenafont = pg.font.SysFont("comicsansms", 25)
 medianofont = pg.font.SysFont("comicsansms", 50)
 largofont = pg.font.SysFont("comicsansms", 80)
@@ -149,6 +150,16 @@ lista_escenarios = [bg_moche,
                     bg_tiahua,
                     bg_wari]
 
+def botones(superficie, color, pos1, pos2, tam1 , tam2):
+    boton = pg.draw.rect(superficie, color, (pos1, pos2, tam1, tam2))
+    return boton
+
+
+def msg_boton(msg, color, posx, posy, ancho, alto, tamano_letra="micro"):
+
+    textSur, textRect = text_objetos(msg, color, tamano_letra)
+    textRect.center = (posx + (ancho/2), posy + (alto/2))
+    screen.blit(textSur, textRect)
 
 def message_to_screen(msg, color, y_displace=0, tamano_letra="pequena"):
 
@@ -164,6 +175,8 @@ def text_objetos(text, color, tamano_letra):
         textSuperficie = medianofont.render(text, True, color)
     elif tamano_letra == "largo":
         textSuperficie = largofont.render(text, True, color)
+    elif tamano_letra == "micro":
+        textSuperficie = microfont.render(text, True, color)
     return textSuperficie, textSuperficie.get_rect()
 
 
